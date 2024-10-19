ab2718c8c7b60c692b3b882e8193b3b146d6658f
Marek Borecki <marek.borecki@finago.com>
2024-10-19 20:32:17 +0300

    add fetching all courses and by id

--- backend/src/main/kotlin/com/memento/course/CourseController.kt
+++ backend/src/main/kotlin/com/memento/course/CourseController.kt
@@ -1,8 +1,7 @@
 package com.memento.course
 
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class CourseController(private val courseService: CourseService) {
@@ -10,6 +9,11 @@ class CourseController(private val courseService: CourseService) {
     @PostMapping("/courses")
     fun addCourse(@RequestBody course: Course): Course = courseService.addCourse(course)
 
+    @GetMapping("/courses")
+    fun getCourses(): List<Course> = courseService.getAll()
 
+    @GetMapping("/courses/{courseId}")
+    fun getCourseById(@PathVariable courseId: Int): Course = courseService.getById(courseId)
+        .orElseThrow { NotFoundException() }
 
 }
--- backend/src/main/kotlin/com/memento/course/CourseService.kt
+++ backend/src/main/kotlin/com/memento/course/CourseService.kt
@@ -1,9 +1,12 @@
 package com.memento.course
 
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class CourseService(private val courseRepository: CourseRepository) {
     fun addCourse(course: Course): Course = courseRepository.save(course)
+    fun getAll(): List<Course> = courseRepository.findAll()
+    fun getById(courseId: Int): Optional<Course> = courseRepository.findById(courseId)
 
 }
